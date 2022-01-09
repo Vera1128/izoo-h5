12953f414333c7a0d0151b7cedad0bfe783092ce
许杨杨 <2406226022@qq.com>
2022-01-09 20:32:47 +0800

    详情页讲解目录列表更新

--- src/modules/audio.ts
+++ src/modules/audio.ts
@@ -27,12 +27,13 @@ export class AudioGlobal {
   }
 
   // audio列表更新
-  audiosInit(audioList: Array<string>, playProgressChangeCb: (progress: ProgressType) => void) {
+  audiosInit(audioList: Array<any>, playProgressChangeCb: (progress: ProgressType) => void) {
+    console.log('audioList', audioList)
     this.audios = {}
     this.audioCurr = null
     audioList.forEach((audio) => {
-      this.audios[audio] = new Audio()
-      const audioTemp = this.audios[audio]
+      this.audios[audio.subId] = new Audio()
+      const audioTemp = this.audios[audio.subId]
       audioTemp.onload = () => {
         console.log('音频加载成功')
       }
@@ -40,21 +41,36 @@ export class AudioGlobal {
         console.log('音频加载失败')
       }
 
-      this.progressMap[audio] = 0
+      this.progressMap[audio.subId] = 0
       audioTemp.ontimeupdate = () => {
         const { currentTime, duration } = audioTemp
         const playProgress = `${(currentTime / duration) * 100}%`
         audioTemp.playProgress = playProgress
-        this.progressMap[audio] = playProgress
+        this.progressMap[audio.subId] = {
+          currentTime,
+          duration,
+          progress: playProgress,
+          isPlay: !audioTemp.paused,
+        }
         playProgressChangeCb({ ...this.progressMap })
       }
-      audioTemp.src = audio
+      audioTemp.src = audio.audioUri
     })
   }
 
   // 播放具体src的音频
-  audioPlay(src: string) {
-    this.audios[src].play()
-    this.audioCurr = this.audios[src]
+  audioPlay(id: string) {
+    if (this.audioCurr) this.audioCurr.audio.pause()
+    if (this.audioCurr?.id !== id) {
+      this.audioCurr = {
+        id,
+        audio: this.audios[id],
+      }
+      this.audioCurr.audio.play()
+    }
+  }
+
+  audioStop() {
+    this.audioCurr.pause()
   }
 }
--- src/pages/detailInfoPage/index.tsx
+++ src/pages/detailInfoPage/index.tsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import SwiperCore, { Pagination, Autoplay } from 'swiper'
 import { notify } from '@tgu/toast'
+import sf from 'seconds-formater'
 
 import FocusOnCom from 'components/FocusOn'
 import EmptyBottom from 'components/EmptyBottom'
@@ -23,6 +24,8 @@ import LocationIcon from 'assets/images/location-icon.png'
 import PriceIcon from 'assets/images/price-icon.png'
 import TimeIcon from 'assets/images/time-icon.png'
 import TagIcon from 'assets/images/tag-icon.png'
+import playIcon from 'assets/images/play-icon.png'
+import pauseIcon from 'assets/images/pause-icon.png'
 
 import 'swiper/css'
 import 'swiper/css/pagination'
@@ -33,26 +36,33 @@ SwiperCore.use([Pagination, Autoplay])
 const Index = ({ history, match, detailInfo, getDetailInfo, setCollectStatus, setDetailInfo }) => {
   const [showShareMask, setShowShareMask] = useState(false)
   const [selectMenu, setSelectMenu] = useState(1)
+  const [playProgress, setPlayProgress] = useState({})
   const {
     params: { id },
   } = match
+  const { catalogList, info, isCollect, isPayment } = detailInfo
   useEffect(() => {
     getDetailInfo(id)
-    const audioList = [
-      'https://izoo-h5.oss-cn-beijing.aliyuncs.com/test.mp3',
-      'https://izoo-h5.oss-cn-beijing.aliyuncs.com/8d76354e48e275caf3e59630fa57993e.m4a',
-    ]
-    AudioGlobal.getInstance().audiosInit(
-      audioList,
-      throttle((progress) => {
-        console.log(progress)
-      }),
-    )
+    return () => {}
   }, [])
-  const clickPlayAudio = () => {
-    AudioGlobal.getInstance().audioPlay('https://izoo-h5.oss-cn-beijing.aliyuncs.com/test.mp3')
+  useEffect(() => {
+    if (catalogList?.length > 0) {
+      const audioList = []
+      catalogList.forEach((item) => {
+        if (item.isAudition) audioList.push(item)
+      })
+      AudioGlobal.getInstance().audiosInit(
+        audioList,
+        throttle((progress) => {
+          setPlayProgress(progress)
+          console.log(progress)
+        }, 1000),
+      )
+    }
+  }, [detailInfo])
+  const clickPlayAudio = (id) => {
+    AudioGlobal.getInstance().audioPlay(id)
   }
-  const { catalogList, info, isCollect, isPayment } = detailInfo
   const backToMainPage = () => {
     history.go(-1)
     setDetailInfo({})
@@ -168,6 +178,44 @@ const Index = ({ history, match, detailInfo, getDetailInfo, setCollectStatus, se
         </div>
         <div onClick={clickPlayAudio}>点击</div>
         <p dangerouslySetInnerHTML={{ __html: info?.content }} />
+        <div className="routeCatalogList">
+          <p>讲解目录</p>
+          {catalogList?.map((item, index) => (
+            <div className="catalogItemContainer" key={item.subId}>
+              <div className="catalogItem">
+                {/* hard code */}
+                {item.isAudition ? (
+                  <>
+                    <div className="num numAudition">试听</div>
+                    <div className="text textAudition">{item.title}</div>
+                    <img
+                      src={playProgress[item.subId]?.isPlay ? pauseIcon : playIcon}
+                      className="playIcon"
+                      onClick={() => clickPlayAudio(item.subId)}
+                    />
+                  </>
+                ) : (
+                  <>
+                    <div className="num">{`0${index + 1}`.slice(-2)}</div>
+                    <div className="text">{item.title}</div>
+                  </>
+                )}
+              </div>
+              {item.isAudition && (
+                <div className="audioProgress">
+                  <span className="currrent">
+                    {sf.convert(Math.round(playProgress[item.subId]?.currentTime || 0)).format('MM:SS')}
+                  </span>
+                  <div className="progressContainer">
+                    <div className="progressBar" style={{ width: playProgress[item.subId]?.progress || 0 }} />
+                    <div className="progressDot" style={{ left: playProgress[item.subId]?.progress || 0 }} />
+                  </div>
+                  <span className="duration">{sf.convert(Math.round(item.duration || 0)).format('MM:SS')}</span>
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
         <EmptyBottom color="white" />
       </div>
 
